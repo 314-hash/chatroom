088129187ce4f5cf8cab4638e70d609c3a834b70
janjanus ladero <najnajoredal@gmail.com>
2025-07-29 19:49:56 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,1 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import million from 'million/compiler';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
-})
